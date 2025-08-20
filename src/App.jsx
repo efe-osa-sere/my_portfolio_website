236b3f8def8b592fd8428e6594cd8630331e9e5a
Efosa <evbuomwanefosa14@gmail.com>
2025-08-20 13:46:23 +0100

    Home section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import "./index.css"
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/section/Home";
+import { About } from "./components/section/About"
 
 const App = () => {
 
@@ -14,11 +16,14 @@ const [menuOpen, setMenuOpen] = useState(false)
 return(
   <>
     {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{" "}
-    <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black text-gray-100`}></div>
+    <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black text-gray-100`}>
 
     <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
- 
+    <Home />
+    <About />
+    
+  </div>
   </>
 )
 }
