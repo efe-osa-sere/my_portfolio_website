e49827e857ef91e800420a7b7bce527612dd6aae
Efosa <evbuomwanefosa14@gmail.com>
2025-08-20 10:12:30 +0100

    nav bar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css"
+import { LoadingScreen } from "./components/LoadingScreen";
+import "./index.css"
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const [isLoaded, setIsLoaded] =useState(false)
+const [menuOpen, setMenuOpen] = useState(false)
+
+
+return(
+  <>
+    {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}{" "}
+    <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100": "opacity-0"} bg-black text-gray-100`}></div>
+
+    <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+ 
+  </>
+)
 }
 
-export default App
+export default App;
